16644704ff2141b5bd943d8693b713730beb9715
AgusDOLARD <agusdolard@gmail.com>
2020-05-29 15:33:14 -0300

    download fixes

--- app.js
+++ app.js
@@ -1,53 +1,63 @@
-const chokidar = require('chokidar')
-const notifier = require('node-notifier')
-const path = require('path')
-const fs = require('fs')
+const chokidar = require("chokidar");
+const notifier = require("node-notifier");
+const path = require("path");
+const fs = require("fs");
 
-const homedir = require('os').homedir()
+const homedir = require("os").homedir();
 
-const originDir = 'Downloads'
-// const destDir = 'Documents';
+const originDir = "Downloads";
 
-const watchFolder = path.join(homedir, originDir)
-const { filetypes } = require('./filetypes')
-// // const finalFolder = path.join(homedir, destDir);
+const watchFolder = path.join(homedir, originDir);
+const { filetypes } = require("./filetypes");
 
 const watcher = chokidar.watch(watchFolder, {
-	persistent: true,
-	ignoreInitial: true
-})
+  persistent: true,
+  ignoreInitial: false,
+  recursive: false,
+  awaitWriteFinish: {
+    stabilityThreshold: 2000,
+    pollInterval: 100,
+  },
+});
 
-watcher.on('add', (filePath) => {
-	const fileName = filePath.split('/').pop()
+const moveFile = (filePath, finalDirectory) => {
+  const file = path.relative(homedir, finalDirectory).split("/");
+  if (fs.existsSync(path.join(homedir, ...file)))
+    fs.mkdirSync(path.join(homedir, ...file));
 
-	filetypes.forEach((element) => {
-		const type = element[0]
-		const newFilePath = path.join(element[1], fileName)
+  try {
+    fs.renameSync(filePath, finalDirectory);
+    const message = `${file.pop()} was moved to ${path.join(...file)}`;
+    console.log(message);
+    notifier.notify({
+      title: "File moved successfully!",
+      message,
+    });
+  } catch (err) {
+    console.error(err);
+    notifier.notify({
+      title: "Error moving file",
+      message: "There it was an error when trying to move the file",
+    });
+  }
+};
 
-		if (fileName.endsWith(type)) {
-			moveFile(filePath, newFilePath)
-		}
-	})
-	// moveFile(filePath, newFilePath);
-})
+watcher.on("ready", () =>
+  notifier.notify({
+    title: "File Organizer",
+    message: "Ready to move! ✌🏿",
+  })
+);
 
-function moveFile(filePath, finalDirectory) {
-	const file = path.relative(homedir, finalDirectory).split('/')
-	if (fs.existsSync(path.join(homedir, ...file)))
-		fs.mkdirSync(path.join(homedir, ...file))
+watcher.on("add", (filePath) => {
+  const fileName = filePath.split("/").pop();
 
-	try {
-		fs.renameSync(filePath, finalDirectory)
-		console.info(`Moved!`)
-		notifier.notify({
-			title: 'File moved successfully!',
-			message: `${file.pop()} was moved to ${path.join(...file)}`
-		})
-	} catch (err) {
-		console.error(err)
-		notifier.notify({
-			title: 'Error moving file',
-			message: 'There it was an error when trying to move the file'
-		})
-	}
-}
+  filetypes.forEach((element) => {
+    const type = element[0];
+    const newFilePath = path.join(element[1], fileName);
+
+    if (fileName.endsWith(type)) {
+      moveFile(filePath, newFilePath);
+    }
+  });
+});
--- filetypes.js
+++ filetypes.js
@@ -1,27 +1,28 @@
 const {
-	getPicturesFolder,
-	getDocumentsFolder,
-	getMusicFolder,
-	getVideosFolder
-} = require('platform-folders');
-const path = require('path');
+  getPicturesFolder,
+  getDocumentsFolder,
+  getMusicFolder,
+  getVideosFolder,
+} = require("platform-folders");
+const path = require("path");
 
 const filetypes = [
-	['jpg', getPicturesFolder()],
-	['png', getPicturesFolder()],
-	['gif', getPicturesFolder()],
-	['tif', getPicturesFolder()],
-	['jpeg', getPicturesFolder()],
-	['raw', getPicturesFolder()],
-	['deb', path.join(getDocumentsFolder(), 'deb')],
-	['m4a', getMusicFolder()],
-	['flac', getMusicFolder()],
-	['mp3', getMusicFolder()],
-	['wav', getMusicFolder()],
-	['mp4', getVideosFolder()],
-	['avi', getVideosFolder()],
-	['mov', getVideosFolder()],
-	['flv', getVideosFolder()]
+  ["jpg", getPicturesFolder()],
+  ["png", getPicturesFolder()],
+  ["gif", getPicturesFolder()],
+  ["tif", getPicturesFolder()],
+  ["jpeg", getPicturesFolder()],
+  ["raw", getPicturesFolder()],
+  ["pdf", path.join(getDocumentsFolder(), "Facultad")],
+  ["iso", path.join(getDocumentsFolder(), "iso")],
+  ["m4a", getMusicFolder()],
+  ["flac", getMusicFolder()],
+  ["mp3", getMusicFolder()],
+  ["wav", getMusicFolder()],
+  ["mp4", getVideosFolder()],
+  ["avi", getVideosFolder()],
+  ["mov", getVideosFolder()],
+  ["flv", getVideosFolder()],
 ];
 
 exports.filetypes = filetypes;
